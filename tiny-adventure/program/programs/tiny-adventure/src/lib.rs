b1c710b271af7ae588cade892fcc67324e559ded
AmirSaber Sharifi <inbox@saberistic.com>
2023-05-31 14:43:12 -0400

    add another dimension to the game

--- tiny-adventure/program/programs/tiny-adventure/src/lib.rs
+++ tiny-adventure/program/programs/tiny-adventure/src/lib.rs
@@ -1,63 +1,76 @@
 use anchor_lang::prelude::*;
 
-declare_id!("2F2K73Sj1ygx4N9ptCegrxEDvGNLCndrsCdmUbcHej3c");
+declare_id!("6GGoL4Zq7rjGs6V5mwr5C4Yozpgz12c2DsmNXR3K3GHa");
 
 #[program]
 pub mod tiny_adventure {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        ctx.accounts.new_game_data_account.player_position = 0;
+        ctx.accounts.new_game_data_account.player_horizontal_position = 0;
+        ctx.accounts.new_game_data_account.player_vertical_position = 0;
         msg!("A Journey Begins!");
-        msg!("o.......");
+        msg!("o.......\n........\n........");
         Ok(())
     }
 
     pub fn move_left(ctx: Context<MoveLeft>) -> Result<()> {
         let game_data_account = &mut ctx.accounts.game_data_account;
-        if game_data_account.player_position == 0 {
-            msg!("You are back at the start.");
+        if game_data_account.player_horizontal_position == 0 {
+            msg!("You are all the way to the left.");
         } else {
-            game_data_account.player_position -= 1;
-            print_player(game_data_account.player_position);
+            game_data_account.player_horizontal_position -= 1;
+            print_player(game_data_account.player_horizontal_position, game_data_account.player_vertical_position);
         }
         Ok(())
     }
 
     pub fn move_right(ctx: Context<MoveRight>) -> Result<()> {
         let game_data_account = &mut ctx.accounts.game_data_account;
-        if game_data_account.player_position == 3 {
-            msg!("You have reached the end! Super!");
+        if game_data_account.player_horizontal_position == 3 {
+            msg!("You are all the way to the right.");
         } else {
-            game_data_account.player_position = game_data_account.player_position + 1;
-            print_player(game_data_account.player_position);
+            game_data_account.player_horizontal_position = game_data_account.player_horizontal_position + 1;
+            print_player(game_data_account.player_horizontal_position, game_data_account.player_vertical_position);
+        }
+        Ok(())
+    }
+
+    pub fn move_up(ctx: Context<MoveUp>) -> Result<()> {
+        let game_data_account = &mut ctx.accounts.game_data_account;
+        if game_data_account.player_vertical_position == 0 {
+            msg!("You are all the way to the top.");
+        } else {
+            game_data_account.player_vertical_position -= 1;
+            print_player(game_data_account.player_horizontal_position, game_data_account.player_vertical_position);
+        }
+        Ok(())
+    }
+
+    pub fn move_down(ctx: Context<MoveDown>) -> Result<()> {
+        let game_data_account = &mut ctx.accounts.game_data_account;
+        if game_data_account.player_vertical_position == 3 {
+            msg!("You are all the way to the bottom.");
+        } else {
+            game_data_account.player_vertical_position += 1;
+            print_player(game_data_account.player_horizontal_position, game_data_account.player_vertical_position);
         }
         Ok(())
     }
 }
 
-fn print_player(player_position: u8) {
-    if player_position == 0 {
-        msg!("A Journey Begins!");
-        msg!("o.......");
-    } else if player_position == 1 {
-        msg!("..o.....");
-    } else if player_position == 2 {
-        msg!("....o...");
-    } else if player_position == 3 {
-        msg!("........\\o/");
-        msg!("You have reached the end! Super!");
-    }
+fn print_player(player_horizontal_position: u8, player_vertical_position: u8) {
+    msg!("You are on {} horizontaly and {} vertically", player_horizontal_position, player_vertical_position);
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(
         init_if_needed,
-        seeds = [b"level1"],
+        seeds = [b"level3"],
         bump,
         payer = signer,
-        space = 8 + 1
+        space = 8 + 1 + 1
     )]
     pub new_game_data_account: Account<'info, GameDataAccount>,
     #[account(mut)]
@@ -77,7 +90,20 @@ pub struct MoveRight<'info> {
     pub game_data_account: Account<'info, GameDataAccount>,
 }
 
+#[derive(Accounts)]
+pub struct MoveUp<'info> {
+    #[account(mut)]
+    pub game_data_account: Account<'info, GameDataAccount>,
+}
+
+#[derive(Accounts)]
+pub struct MoveDown<'info> {
+    #[account(mut)]
+    pub game_data_account: Account<'info, GameDataAccount>,
+}
+
 #[account]
 pub struct GameDataAccount {
-    player_position: u8,
+    player_vertical_position: u8,
+    player_horizontal_position: u8,
 }
--- tiny-adventure/program/tests/tiny-adventure.ts
+++ tiny-adventure/program/tests/tiny-adventure.ts
@@ -12,7 +12,7 @@ describe("tiny-adventure", () => {
 
   // PDA for the game data account
   const [newGameDataAccount] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from("level1", "utf8")],
+    [Buffer.from("level3", "utf8")],
     program.programId
   )
 
@@ -31,11 +31,16 @@ describe("tiny-adventure", () => {
     const gameDataAccount = await program.account.gameDataAccount.fetch(
       newGameDataAccount
     )
-    assert(gameDataAccount.playerPosition == 0)
+    assert(gameDataAccount.playerHorizontalPosition == 0)
+    assert(gameDataAccount.playerVerticalPosition == 0)
 
     console.log(
-      "Player position is:",
-      gameDataAccount.playerPosition.toString()
+      "Player horizontal position is:",
+      gameDataAccount.playerHorizontalPosition.toString()
+    )
+    console.log(
+      "Player vertical position is:",
+      gameDataAccount.playerVerticalPosition.toString()
     )
   })
 
@@ -55,11 +60,11 @@ describe("tiny-adventure", () => {
       newGameDataAccount
     )
 
-    assert(gameDataAccount.playerPosition == 3)
+    assert(gameDataAccount.playerHorizontalPosition == 3)
 
     console.log(
-      "Player position is:",
-      gameDataAccount.playerPosition.toString()
+      "Player horizontal position is:",
+      gameDataAccount.playerHorizontalPosition.toString()
     )
   })
 
@@ -79,11 +84,59 @@ describe("tiny-adventure", () => {
       newGameDataAccount
     )
 
-    assert(gameDataAccount.playerPosition == 0)
+    assert(gameDataAccount.playerHorizontalPosition == 0)
 
     console.log(
-      "Player position is:",
-      gameDataAccount.playerPosition.toString()
+      "Player horizontal position is:",
+      gameDataAccount.playerHorizontalPosition.toString()
+    )
+  })
+
+  it("Run Down", async () => {
+    // Move left 3 times
+    for (let i = 0; i < 3; i++) {
+      const tx = await program.methods
+        .moveDown()
+        .accounts({
+          gameDataAccount: newGameDataAccount,
+        })
+        .rpc()
+    }
+
+    // Fetch the game data account
+    const gameDataAccount = await program.account.gameDataAccount.fetch(
+      newGameDataAccount
+    )
+
+    assert(gameDataAccount.playerVerticalPosition == 3)
+
+    console.log(
+      "Player vertical position is:",
+      gameDataAccount.playerVerticalPosition.toString()
+    )
+  })
+
+  it("Run Up", async () => {
+    // Move left 3 times
+    for (let i = 0; i < 3; i++) {
+      const tx = await program.methods
+        .moveUp()
+        .accounts({
+          gameDataAccount: newGameDataAccount,
+        })
+        .rpc()
+    }
+
+    // Fetch the game data account
+    const gameDataAccount = await program.account.gameDataAccount.fetch(
+      newGameDataAccount
+    )
+
+    assert(gameDataAccount.playerVerticalPosition == 0)
+
+    console.log(
+      "Player vertical position is:",
+      gameDataAccount.playerVerticalPosition.toString()
     )
   })
 })
